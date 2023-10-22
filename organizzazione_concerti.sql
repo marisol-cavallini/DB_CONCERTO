a66cf9e9c2a88c03d5ec9fcb186742d31b9ed30c
marisol-cavallini <94352597+marisol-cavallini@users.noreply.github.com>
2023-10-22 21:59:38 +0200

    Delete organizzazione_concerti.sql

--- organizzazione_concerti.sql
+++
@@ -1,9 +0,0 @@
-create database if not exists organizzazione_concerto;
-create table if not exists organizzazione_concerto.concerti(
-id int not null auto_increment,
-codice_concerto varchar(20),
-titolo_concerto varchar(20),
-descrizione_concerto varchar (100),
-data_concerto varchar(20),
-primary key (id)
-);
